2572d8f940569024a169966315921ed1002c935e
Pedro Cazala <cazalapedro@gmail.com>
2022-07-07 15:20:46 -0300

    probando para heroku con un archivo server2.js mas sencillo version 2 con iconos

--- server2.js
+++ server2.js
@@ -4,8 +4,8 @@ const app = express()
 
 const PORT = process.env.PORT || 2020
 
-const server = app.listen(PORT,()=>console.log(`Corriendo en el puerto ${PORT}`) )
+const server = app.listen(PORT,()=>console.log(`🔥Corriendo en el puerto ${PORT}`) )
 
 app.get('/',(req,res) => {
-    res.send('ok desde server 2')
+    res.send('ok desde server 2🏳️‍🌈')
 })
